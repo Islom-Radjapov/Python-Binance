547f33a7b9a85f53fc3ad1746c32a34f426e9e2c
Radjapov Islom <radjapov_islom@mail.ru>
2023-04-27 17:50:25 +0500

    connect binance good

--- work.py
+++ work.py
@@ -1,5 +1,15 @@
-import math
+from binance.client import Client
+import config
 
-y = 1
-for x in range(11):
-    print(math.floor(x * 1.618))
+client = Client(config.api_key, config.api_secret)
+print("login")
+
+# info = client.get_symbol_info("BTCUSDT")
+# for x, y in info.items():
+#     print(x, y)
+info = client.get_account()
+balans = info['balances']
+for x in balans:
+    # print(x)
+    if float(x['free']) > 0:
+        print(x)
