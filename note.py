4962054f3c228910b69849abfe1f60cbc3bb2179
Radjapov Islom <radjapov_islom@mail.ru>
2023-05-02 12:28:34 +0500

    'backtest'

--- note.py
+++ note.py
@@ -19,3 +19,29 @@ def klines(symbol):
 
 
 ta.trend.macd(df.close).iloc[-1], ta.trend.macd_signal(df.close).iloc[-1], bnb_macd.iloc[-1]
+
+
+class MACDStrategy:
+    def __init__(self, fast_period=12, slow_period=26, signal_period=9):
+        self.fast_period = fast_period
+        self.slow_period = slow_period
+        self.signal_period = signal_period
+
+    def generate_signals(self, df):
+        # Calculate MACD and Signal line values
+        macd_indicator = ta.trend.MACD(df['Close'], self.fast_period, self.slow_period, self.signal_period)
+        macd = macd_indicator.macd()
+        signal = macd_indicator.macd_signal()
+
+        # Generate buy and sell signals based on MACD crossover with Signal line
+        buy_signals = (macd > signal) & (macd.shift(1) < signal.shift(1))
+        sell_signals = (macd < signal) & (macd.shift(1) > signal.shift(1))
+
+        # Combine signals into a single DataFrame
+        signals = pd.DataFrame(index=df.index)
+        signals['positions'] = 0
+        signals.loc[buy_signals, 'positions'] = 1
+        signals.loc[sell_signals, 'positions'] = -1
+
+        return signals
+
--- work.py
+++ work.py
@@ -1,15 +1,28 @@
 from binance.client import Client
 import config
+from datetime import datetime, timedelta
 
-client = Client(config.api_key, config.api_secret)
-print("login")
+# client = Client(config.api_key, config.api_secret)
+# print("login")
+#
+# # info = client.get_symbol_info("BTCUSDT")
+# # for x, y in info.items():
+# #     print(x, y)
+# info = client.get_account()
+# balans = info['balances']
+# for x in balans:
+#     # print(x)
+#     if float(x['free']) > 0:
+#         print(x)
 
-# info = client.get_symbol_info("BTCUSDT")
-# for x, y in info.items():
-#     print(x, y)
-info = client.get_account()
-balans = info['balances']
-for x in balans:
-    # print(x)
-    if float(x['free']) > 0:
-        print(x)
+
+def get_candle_1h_before(time):
+    start = str(time - timedelta(hours=5))
+    end = str(time)
+    df = pd.DataFrame(client.get_historical_klines(symbol="BNBUSDT", interval='1h', start_str=start, end_str=end))
+    df = df.iloc[:, :5]
+    df.columns = ['Time', 'Open', 'High', 'Low', 'Close']
+    df = df.set_index('Time')
+    df.index = pd.to_datetime(df.index, unit='ms')
+    df = df.astype(float)
+    return df
